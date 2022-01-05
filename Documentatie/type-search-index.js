092c3c91cd6b51447ef6dd27ef5470adfe8b7336
Stefaniammaria <92268724+Stefaniammaria@users.noreply.github.com>
2022-01-05 12:54:14 +0200

    Add files via upload

---
+++ Documentatie/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Board"},{"p":"<Unnamed>","l":"CountTime"},{"p":"<Unnamed>","l":"Minesweeper"},{"p":"<Unnamed>","l":"Timer"}];updateSearchResults();
